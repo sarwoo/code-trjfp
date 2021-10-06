81f7290824d61c0f8883a823fe66144a0e17d986
Sarah Wood <spiral464@gmail.com>
2021-10-06 22:22:42 +0100

    added main file

---
+++ main.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def main():
+    df = pd.read_csv("./data/Intercept List 06_10_2021.csv")
+    df["Date Intercepted"] = pd.to_datetime(df["Date Intercepted"], dayfirst=True)
+    grams = df.groupby(df['Date Intercepted'].dt.month)['Weight (in grams)'].sum()
+    n_months = grams.index.tolist()
+    result = [g / 1_000_000 for g in grams]
+    months = month_list(n_months)
+
+    plt.bar(months, result, color='green')
+
+    plt.ylabel("Tonnes")
+    plt.title("Monthly Intercepts 2021")
+    plt.show()
+
+def month_list(month_nums):
+    months= { 1: "Jan",2: "Feb",3: "Mar",4: "Apr",5: "may",6: "Jun",7: "Jul",8: "Aug",9: "Sep",10: "Oct",11: "Nov",12: "Dec"}
+    return [months[num] for num in month_nums]
+    
+
+if __name__ == "__main__":
+    main()
