c95db987941bcb32214292d0d23a51ce0ffc3d50
Sarah Wood <spiral464@gmail.com>
2021-10-25 23:01:10 +0100

    save data init

--- application/console-source.py
+++ application/console-source.py
@@ -8,7 +8,7 @@ def parse_category(num: int):
 inp_name = input("Enter company name of the source: ")
 inp_category_num = int(input("Enter source category number [1]:hospitality, [2]:private, [3]:rdc or [4]:retail: "))
 
-
 category = parse_category(inp_category_num)
-Source(inp_name, category)
-print(Source.all)
+source = Source(inp_name, category)
+source.save()
+
--- application/source.py
+++ application/source.py
@@ -4,22 +4,16 @@ from enum import Enum
 class Source:
 
     class Category(Enum):
-        HOSPITALITY = 1
-        PRIVATE = 2
-        RDC = 3
-        RETAIL = 4
-        
-    all = []
+        HOSPITALITY = 'Hospitality'
+        PRIVATE = 'Private'
+        RDC = 'RDC'
+        RETAIL = 'Retail'
 
     def __init__(self, name: str, catagory: Category) -> None:
-
         assert type(name) == str
         assert type(catagory) == Source.Category
-
         self.__name = name
-        self.__category = catagory
-
-        Source.all.append(self)
+        self.__category = catagory.value
 
     @property
     def name(self):
@@ -29,8 +23,9 @@ class Source:
     def category(self):
         return self.__category
 
+    def save(self):
+        with open('data.csv', 'a') as file:
+            file.write(f'"{self.name}","{self.category}"\n')
+
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}('{self.name}', Source.{self.category})"
-
-source = Source('Aldi', Source.Category.RETAIL)
-print(source)
